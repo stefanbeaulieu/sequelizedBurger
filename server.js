2937749d354447d69fc74b63d38435827ce24685
Stefan Beaulieu <beaulieustefan@gmail.com>
2017-02-12 22:32:31 -0500

    debuggind

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Requiring our models for syncing
+var db = require("./models");
+
+// Static directory
 app.use(express.static(process.cwd() + "/public"));
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
@@ -27,4 +31,9 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(PORT);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
